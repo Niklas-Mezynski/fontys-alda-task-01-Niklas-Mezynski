05f368cb487a2e5742f045313fc0bcb0596fa6a1
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-09-02 09:51:24 +0200

    Added factory test

---
+++ simpletime/simpletimeimpl/src/test/java/simpletimeimpl/FactoryTest.java
@@ -0,0 +1,33 @@
+package simpletimeimpl;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import simpletimeapi.AbstractAPFactory;
+import simpletimeapi.Duration;
+import simpletimeapi.Time;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+
+public class FactoryTest {
+
+    static AbstractAPFactory factory = ServiceFinder.getFactory();
+
+    @Test
+    void t01createTime() {
+        Time time = factory.createTime(2, 30);
+        assertThat(time).isNotNull();
+    }
+
+    @Test
+    void t02createDuration() {
+        Duration duration1 = factory.createDuration(45);
+        Duration duration2 = factory.createDuration(4, 25);
+
+        SoftAssertions.assertSoftly((s -> {
+            s.assertThat(duration1).isNotNull();
+            s.assertThat(duration2).isNotNull();
+        }));
+    }
+
+
+}
